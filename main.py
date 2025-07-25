104b1d3d4f9eb7642e23d5a6b5a1f130bfa1690e
ProNet7 <mailetomohsen@gmail.com>
2025-07-25 16:01:48 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import asyncio
 import os
-from telethon import TelegramClient
 
-api_id = int(os.getenv("API_ID"))
-api_hash = os.getenv("API_HASH")
-phone = os.getenv("PHONE_NUMBER")
-
-client = TelegramClient("anon", api_id, api_hash)
-
-async def main():
-    await client.start(phone=phone)
-    print("✅ Logged in successfully.")
-
-asyncio.run(main())
+print("🧪 TESTING SECRETS:")
+print("API_ID:", os.getenv("API_ID"))
+print("API_HASH:", os.getenv("API_HASH"))
+print("PHONE_NUMBER:", os.getenv("PHONE_NUMBER"))
